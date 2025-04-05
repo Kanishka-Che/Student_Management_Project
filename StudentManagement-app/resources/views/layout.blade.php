823c2c931e7b8367de307c17aed96070a4a6e9b6
Kanishka-Che <akcadikari@std.appsc.sab.ac.lk>
2025-04-05 23:40:31 +0530

    initial commit 7

--- StudentManagement-app/resources/views/layout.blade.php
+++ StudentManagement-app/resources/views/layout.blade.php
@@ -4,7 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Student management system </title>
+      <style>
+          .sidebar {
+                margin: 0;
+                padding: 0;
+                width: 200px;
+                background-color: #f1f1f1;
+                position: fixed;
+                height: 100%;
+                overflow: auto;
+              }
+
+              .sidebar a {
+                display: block;
+                color: black;
+                padding: 16px;
+                text-decoration: none;
+              }
+
+              
+              .sidebar a.active {
+                background-color: #04AA6D;
+                color: white;
+              }
+
+              
+              .sidebar a:hover:not(.active) {
+                background-color: #555;
+                color: white;
+              }
+
+              div.content {
+                margin-left: 200px;
+                padding: 1px 16px;
+                height: 1000px;
+              }
+
+              @media screen and (max-width: 700px) {
+                .sidebar {
+                  width: 100%;
+                  height: auto;
+                  position: relative;
+                }
+                .sidebar a {float: left;}
+                div.content {margin-left: 0;}
+              }
+
+              @media screen and (max-width: 400px) {
+                .sidebar a {
+                  text-align: center;
+                  float: none;
+                }
+              }
+
+
+      </style>  
+
+
+
+
+
 </head>
 <body>
     <div class="container">
@@ -17,18 +77,23 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-   
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
 </nav>
 </div>
          </div>
 
+         <div class="row">
+           <div class= "col-md-3">
+           <div class="sidebar">
+              <a class="active" href="#home">Home</a>
+              <a href="#news">News</a>
+              <a href="#contact">Contact</a>
+              <a href="#about">About</a>
+            </div>
+
+            <div class="col-md-9">
+                   <div class="content">
+                          @yield(`content`)
+            </div>
 
 
 </div>
